a830edfa6b4dd682f5713404adc03e313be4c5f3
amit <gautamamits95@gmail.com>
2020-02-06 01:23:04 +0530

    Updates

---
+++ precache-manifest.e72ac7fbcb94c5e341fb47809517ea45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113045b87754ec31dca5f2eb7f18ab17",
+    "url": "programming-gems/index.html"
+  },
+  {
+    "revision": "6070815895bc1632ef8a",
+    "url": "programming-gems/static/css/main.cedbbc82.chunk.css"
+  },
+  {
+    "revision": "fb7bda20bc0e68be67bf",
+    "url": "programming-gems/static/js/2.a08baf53.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "programming-gems/static/js/2.a08baf53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6070815895bc1632ef8a",
+    "url": "programming-gems/static/js/main.48931860.chunk.js"
+  },
+  {
+    "revision": "6ab4fd62bc7fcdc9684a",
+    "url": "programming-gems/static/js/runtime-main.5adb8e65.js"
+  }
+]);
